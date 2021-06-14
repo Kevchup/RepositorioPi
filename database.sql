d7f2bbd13137f103dab10c1a2af4e8508e3cd5b8
Kevchup <81330494+Kevchup@users.noreply.github.com>
2021-06-14 12:37:49 -0500

    commit tablas bd

--- database.sql
+++ database.sql
@@ -1,31 +1,18 @@
 CREATE TABLE usuario(
-	ID_U int (11) not null,
+	ID int (11) not null,
 	nombre varchar(40) null,
 	apellido varchar(40) null,
 	telefono varchar(10) null,
 	email varchar(30) null,
 	contraseña varchar(10) null,
 	CONSTRAINT pk_usuario
-	PRIMARY KEY (ID_U)
-)
-
-
-
-CREATE TABLE productor(
-	ID_P int (11) not null,
-	nombre varchar(40) null,
-	apellido varchar(40) null,
-	telefono varchar(10) null,
-	email varchar(30) null,
-	contraseña varchar(10) null,
-	CONSTRAINT pk_productor
-	PRIMARY KEY (ID_P)
+	PRIMARY KEY (ID)
 )
 
 
 CREATE TABLE producto(
 	codigo int (11) not null AUTO_INCREMENT,
-	ID_P int (11) not null,
+	ID int (11) not null,
 	titulo varchar(50) null,
 	descrip varchar(100) null,
 	imagen varchar(200) null,
@@ -34,13 +21,13 @@ CREATE TABLE producto(
 	CONSTRAINT pk_producto
 	PRIMARY KEY (codigo),
 	CONSTRAINT fk_producto
-	FOREIGN KEY (ID_P) REFERENCES productor (ID_P)	
+	FOREIGN KEY (ID) REFERENCES productor (ID)	
 )
 
 	
 CREATE TABLE tipou(
-	ID_P int (11) not null,
+	ID int (11) not null,
 	nombre varchar(40) null,
 	CONSTRAINT pk_productor
-	PRIMARY KEY (ID_P)
+	PRIMARY KEY (ID)
 )
